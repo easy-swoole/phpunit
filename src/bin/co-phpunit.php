b14c40a3ad581185daff33a3cddb20e16e5f8b60
yf <291323003@qq.com>
2019-01-06 22:03:29 +0800

    up

---
+++ src/bin/co-phpunit.php
@@ -0,0 +1,12 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-01-06
+ * Time: 21:49
+ */
+require_once __DIR__.'/../../vendor/autoload.php';
+go(function() {
+    require_once __DIR__.'/../../vendor/bin/phpunit';
+});
