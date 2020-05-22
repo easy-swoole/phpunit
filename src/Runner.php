ccff6efa0ef2d25a0ab0383c92e167b4f418b518
如果的如果 <admin@fosuss.com>
2020-05-22 22:57:46 +0800

    up fix

--- src/Runner.php
+++ src/Runner.php
@@ -7,22 +7,32 @@ namespace EasySwoole\Phpunit;
 use PHPUnit\TextUI\Command;
 use Swoole\ExitException;
 use Swoole\Timer;
+use Swoole\Coroutine\Scheduler;
 
 class Runner
 {
-    public static function run()
+    public static function run($noCoroutine = true)
     {
-        try{
-            Command::main(false);
-        }catch (\Throwable $throwable){
-            /*
-             * 屏蔽swoole exit报错
-             */
-            if(!$throwable instanceof ExitException){
-                throw $throwable;
+        if($noCoroutine){
+            try{
+                Command::main(false);
+            }catch (\Throwable $throwable){
+                /*
+                 * 屏蔽swoole exit报错
+                 */
+                if(!$throwable instanceof ExitException){
+                    throw $throwable;
+                }
+            }finally{
+                Timer::clearAll();
             }
-        }finally{
-            Timer::clearAll();
+        }else{
+            $scheduler = new Scheduler();
+            $scheduler->add(function() {
+                Runner::run();
+            });
+            $scheduler->start();
         }
+
     }
 }
